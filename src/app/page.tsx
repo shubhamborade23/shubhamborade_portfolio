b49cb9a53a7f7e3933ca5adf2cfb5138a531ed0a
Shubham Borade <boradeshubham02@gmail.com>
2024-10-04 20:10:44 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,253 @@
+import { HackathonCard } from "@/components/hackathon-card";
+import BlurFade from "@/components/magicui/blur-fade";
+import BlurFadeText from "@/components/magicui/blur-fade-text";
+import { ProjectCard } from "@/components/project-card";
+import { ResumeCard } from "@/components/resume-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { DATA } from "@/data/resume";
+import Link from "next/link";
+import Markdown from "react-markdown";
+import { IconCloud } from "@/components/magicui/icon-cloud";
+import { HyperText } from "@/components/magicui/hyper-text";
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "salesforce",
+  "react",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "firebase",
+  "jquery",
+  "jest",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "figma",
+  "shopify",
+  "mongodb",
+  "liquid",
+  "isml",
+  "ajax",
+  "json",
+  "scss",
+  "notepad++",
+  "sublime",
+  "jenkins",
+];
+
+const BLUR_FADE_DELAY = 0.04;
+
+export default function Page() {
+  return (
+    <main className="flex flex-col min-h-[100dvh] space-y-10">
+      <section id="hero">
+        <div className="mx-auto w-full max-w-2xl space-y-8">
+          <div className="gap-10 flex items-center justify-between">
+            <BlurFade delay={BLUR_FADE_DELAY}>
+              <Avatar className="size-28 border">
+                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
+                <AvatarFallback>{DATA.initials}</AvatarFallback>
+              </Avatar>
+            </BlurFade>
+            <div className="flex-col flex flex-1 space-y-1.5">
+              <HyperText
+                className="text-3xl font-bold font-sans tracking-tighter sm:text-5xl xl:text-5xl/none"
+                text={`Hi, I'm ${DATA.name.split(" ")[0]}`}
+              />
+              <BlurFadeText
+                className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert"
+                delay={BLUR_FADE_DELAY}
+                text={DATA.description}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="about">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-xl font-bold">About</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className="prose max-w-full text-pretty font-sans xs:text-xl/relaxed text-muted-foreground dark:prose-invert">
+            {DATA.summary}
+          </Markdown>
+        </BlurFade>
+      </section>
+      {/* <section id="work">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 5}>
+            <h2 className="text-xl font-bold">Work Experience</h2>
+          </BlurFade>
+          {DATA.work.map((work, id) => (
+            <BlurFade
+              key={work.company}
+              delay={BLUR_FADE_DELAY * 6 + id * 0.05}
+            >
+              <ResumeCard
+                key={work.company}
+                logoUrl={work.logoUrl}
+                altText={work.company}
+                title={work.company}
+                subtitle={work.title}
+                href={work.href}
+                badges={work.badges}
+                period={`${work.start} - ${work.end ?? "Present"}`}
+                description={work.description}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section> */}
+      <section id="education">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 7}>
+            <h2 className="text-xl font-bold">Education</h2>
+          </BlurFade>
+          {DATA.education.map((education, id) => (
+            <BlurFade
+              key={education.school}
+              delay={BLUR_FADE_DELAY * 8 + id * 0.05}
+            >
+              <ResumeCard
+                key={education.school}
+                href={education.href}
+                logoUrl={education.logoUrl}
+                altText={education.school}
+                title={education.school}
+                subtitle={education.degree}
+                period={`${education.start} - ${education.end}`}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+      <section id="skills">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 9}>
+            <h2 className="text-xl font-bold">Skills</h2>
+          </BlurFade>
+          <div className="flex flex-wrap gap-1">
+            {DATA.skills.map((skill, id) => (
+              <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+                <Badge key={skill}>{skill}</Badge>
+              </BlurFade>
+            ))}
+          </div>
+          <IconCloud iconSlugs={slugs} />
+        </div>
+      </section>
+      <section id="projects">
+        <div className="space-y-12 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  My Projects
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  I like building things
+                </h2>
+                <p className="text-muted-foreground xs:text-xl/relaxed">
+                  I&apos;ve worked on a variety of projects, from simple
+                  websites to complex web applications.
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+            {DATA.projects.map((project, id) => (
+              <BlurFade
+                key={project.title}
+                delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+              >
+                <ProjectCard
+                  href={project.href}
+                  key={project.title}
+                  title={project.title}
+                  description={project.description}
+                  dates={project.dates}
+                  tags={project.technologies}
+                  image={project.image}
+                  video={project.video}
+                  links={project.links}
+                />
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section id="hackathons">
+        <div className="space-y-12 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 13}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  Work Experience
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Organisations Details
+                </h2>
+                <p className="xs:text-xl/relaxed text-muted-foreground">
+                  I worked in {DATA.organizations.length} organizations so far.
+                  I have learned so many things which helped me to grow as a
+                  software engineer to develop skills and continues
+                  improvements.
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <BlurFade delay={BLUR_FADE_DELAY * 14}>
+            <ul className="mb-4 ml-4 divide-y divide-dashed border-l">
+              {DATA.organizations.map((project, id) => (
+                <BlurFade
+                  key={project.title + project.dates}
+                  delay={BLUR_FADE_DELAY * 15 + id * 0.05}
+                >
+                  <HackathonCard
+                    title={project.title}
+                    description={project.description}
+                    location={project.location}
+                    dates={project.dates}
+                    image={project.image}
+                    links={project.links}
+                  />
+                </BlurFade>
+              ))}
+            </ul>
+          </BlurFade>
+        </div>
+      </section>
+      <section id="contact">
+        <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 16}>
+            <div className="space-y-3">
+              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                Contact
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Get in Touch
+              </h2>
+              <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Want to chat? Just shoot me a dm{" "}
+                <Link
+                  href={DATA.contact.social.WhatsApp.url}
+                  className="text-blue-500 hover:underline"
+                >
+                  with a direct question on twitter
+                </Link>{" "}
+                and I&apos;ll respond whenever I can. I will ignore all
+                soliciting.
+              </p>
+            </div>
+          </BlurFade>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/data/resume.tsx
@@ -0,0 +1,384 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Shubham Borade",
+  initials: "DV",
+  url: "https://dillion.io",
+  location: "Mumbai, IND",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    "A full Stack Software Engineer with expertise in both frontend and backend development. Capable of handling frontend and backend tasks, including UI design, database integration, and server-side development. Experienced in implementing various payment gateways and customizing search engines for enhanced user experiences. Capable of delivering efficient and user-friendly solutions for diverse projects.",
+  summary:
+    "I pursued a degree in the 2020 and started my career as a software engineer to go fulltime into building and scaling applications.",
+  avatarUrl: "/me.jpg",
+  skills: [
+    "SFCC",
+    "SFCC CMS",
+    "Shopify",
+    "ISML",
+    "SFRA",
+    "PWA",
+    "Liquid",
+    "ReactJs",
+    "Typescript",
+    "Node.js",
+    "Express.js",
+    "Javascript",
+    "jQuery",
+    "SCSS",
+    "HTML5",
+    "CSS3",
+    "Jira",
+    "Git",
+    "Github",
+    "Bitbucket",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    // { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "boradeshubham02@gmail.com",
+    tel: "+91 7506202934",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/shubhamborade23",
+        icon: Icons.github,
+        navbar: true,
+        isExternal: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/shubham-borade-a162a5178/",
+        icon: Icons.linkedin,
+        navbar: true,
+        isExternal: true,
+      },
+      WhatsApp: {
+        name: "X",
+        url: "https://wa.link/lpzye4",
+        icon: Icons.whatsapp,
+        navbar: true,
+        isExternal: true,
+      },
+      Email: {
+        name: "Email",
+        url: "mailto:boradeshubham02@gmail.com.com",
+        icon: Icons.email,
+        navbar: true,
+        isExternal: true,
+      },
+    },
+  },
+
+  // work: [
+  //   {
+  //     company: "Atomic Finance",
+  //     href: "https://atomic.finance",
+  //     badges: [],
+  //     location: "Remote",
+  //     title: "Bitcoin Protocol Engineer",
+  //     logoUrl: "/atomic.png",
+  //     start: "May 2021",
+  //     end: "Oct 2022",
+  //     description:
+  //       "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+  //   },
+  //   {
+  //     company: "Shopify",
+  //     badges: [],
+  //     href: "https://shopify.com",
+  //     location: "Remote",
+  //     title: "Software Engineer",
+  //     logoUrl: "/shopify.svg",
+  //     start: "January 2021",
+  //     end: "April 2021",
+  //     description:
+  //       "Implemented a custom Kubernetes controller in Go to automate the deployment of MySQL and ProxySQL custom resources in order to enable 2,000+ internal developers to instantly deploy their app databases to production. Wrote several scripts in Go to automate MySQL database failovers while maintaining master-slave replication topologies and keeping Zookeeper nodes consistent with changes.",
+  //   },
+  //   {
+  //     company: "Nvidia",
+  //     href: "https://nvidia.com/",
+  //     badges: [],
+  //     location: "Santa Clara, CA",
+  //     title: "Software Engineer",
+  //     logoUrl: "/nvidia.png",
+  //     start: "January 2020",
+  //     end: "April 2020",
+  //     description:
+  //       "Architected and wrote the entire MVP of the GeForce Now Cloud Gaming internal admin and A/B testing dashboard using React, Redux, TypeScript, and Python.",
+  //   },
+  //   {
+  //     company: "Splunk",
+  //     href: "https://splunk.com",
+  //     badges: [],
+  //     location: "San Jose, CA",
+  //     title: "Software Engineer",
+  //     logoUrl: "/splunk.svg",
+  //     start: "January 2019",
+  //     end: "April 2019",
+  //     description:
+  //       "Co-developed a prototype iOS app with another intern in Swift for the new Splunk Phantom security orchestration product (later publicly demoed and launched at .conf annual conference in Las Vegas). Implemented a realtime service for the iOS app in Django (Python) and C++; serialized data using protobufs transmitted over gRPC resulting in an approximate 500% increase in data throughput.",
+  //   },
+  //   {
+  //     company: "Lime",
+  //     href: "https://li.me/",
+  //     badges: [],
+  //     location: "San Francisco, CA",
+  //     title: "Software Engineer",
+  //     logoUrl: "/lime.svg",
+  //     start: "January 2018",
+  //     end: "April 2018",
+  //     description:
+  //       "Proposed and implemented an internal ruby API for sending/receiving commands to scooters over LTE networks. Developed a fully automated bike firmware update system to handle asynchronous firmware updates of over 100,000+ scooters worldwide, and provide progress reports in real-time using React, Ruby on Rails, PostgreSQL and AWS EC2 saving hundreds of developer hours.",
+  //   },
+  //   {
+  //     company: "Mitre Media",
+  //     href: "https://mitremedia.com/",
+  //     badges: [],
+  //     location: "Toronto, ON",
+  //     title: "Software Engineer",
+  //     logoUrl: "/mitremedia.png",
+  //     start: "May 2017",
+  //     end: "August 2017",
+  //     description:
+  //       "Designed and implemented a robust password encryption and browser cookie storage system in Ruby on Rails. Leveraged the Yahoo finance API to develop the dividend.com equity screener",
+  //   },
+  // ],
+  education: [
+    {
+      school: "Dr. DY Patil Vidyapeeth, Pune",
+      href: "https://www.dypatilonline.com/onlinemba/pune/?utm_source=unipro&utm_medium=search&utm_campaign=Brand_Phrase_Pune&utm_matchtype=b&utm_network=g&utm_keyword=dy%20patil%20pune&utm_device=c&gad_source=1&gclid=Cj0KCQjwu-63BhC9ARIsAMMTLXT_LjXgudlX6DffXWkzCDKAlt_02bjZjY4Y_X6Le1RKBuIxIcL1AZEaAjQ4EALw_wcB#",
+      degree: "MBA in Information Technology",
+      logoUrl: "https://www.dypatilonline.com/onlinemba/pune/images/logo.png",
+      start: "2022",
+      end: "2024",
+    },
+    {
+      school: "PVPPCOE, Mumbai",
+      href: "https://pvppcoe.ac.in/",
+      degree: "Bachelor's Degree of Computer Engineering (BE)",
+      logoUrl:
+        "https://pvppcoe.ac.in/wp-content/uploads/2022/12/VISUAL-ART-LOGO-1-1024x1024.png",
+      start: "2017",
+      end: "2020",
+    },
+    {
+      school: "KVMIT Ploytechnic, Mumbai",
+      href: "https://www.kvmt.co.in/",
+      degree: "Diploma in Information Technology",
+      logoUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlT4WLXDzRzsRcz1gy_ae0Kzs4vo5ANoLroQ&s",
+      start: "2014",
+      end: "2017",
+    },
+    {
+      school: "Sadhana Vidyalaya, Mumbai",
+      href: "https://www.justdial.com/Mumbai/Sadhana-Vidyalaya-Pre-Primary-School-Near-Vallabhadas-Vidyalaya-Sion/022PXX22-XX22-180205181925-U9Q2_BZDET",
+      degree: "SSC",
+      logoUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLeQOADTjqb99_qE5BsnNfD0gUqJsGPFt2QfqKwgRq_TV8Iexq2BQ1Hz2X3rKll-S-4CQ&usqp=CAU",
+      start: "2004",
+      end: "2014",
+    },
+  ],
+  projects: [
+    {
+      title: "ASDA",
+      href: "https://groceries.asda.com/",
+      dates: "April 2024 - Present",
+      active: true,
+      description:
+        "Designed, developed and sold animated UI components for developers.",
+      technologies: [
+        "SFCC",
+        "PWA",
+        "ReactJS",
+        "Typescript",
+        "Javascript",
+        "SCSS",
+        "Headless",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://magicui.design",
+          icon: <Icons.globe className="size-3" />,
+        },
+        // {
+        //   type: "Source",
+        //   href: "https://github.com/magicuidesign/magicui",
+        //   icon: <Icons.github className="size-3" />,
+        // },
+      ],
+      image: "",
+      video: "/asda.mp4",
+    },
+    {
+      title: "Titan IRTH & SKINN",
+      href: "https://llm.report",
+      dates: "January 2024 - April 2024",
+      active: true,
+      description:
+        "Developed the features and enhancement after migrating the project to the SFCC platform.",
+      technologies: [
+        "SFCC",
+        "Javascript",
+        "ISML",
+        "SCSS",
+        "jQuery",
+        "Bootstrap",
+        "Demandware",
+        "Github",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://www.skinn.in/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        // {
+        //   type: "Source",
+        //   href: "https://github.com/dillionverma/llm.report",
+        //   icon: <Icons.github className="size-3" />,
+        // },
+      ],
+      image: "",
+      video: "/skinn.mp4",
+    },
+    {
+      title: "Pet Supermarket",
+      href: "https://www.petsupermarket.com",
+      dates: "December 2023 - January 2024",
+      active: true,
+      description:
+        "Developed multiple features, services, and enahnced landing pages and added new features to it.",
+      technologies: [
+        "SFCC",
+        "Javascript",
+        "Service framework",
+        "ISML",
+        "Business manager",
+        "SCSS",
+        "jQuery",
+        "Bootstrap",
+        "Demandware",
+        "Bitbucket",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://www.petsupermarket.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "/petsm.mp4",
+    },
+    {
+      title: "Aerin Beauty",
+      href: "https://www.aerin.com/",
+      dates: "May 2021 - December 2021",
+      active: true,
+      description:
+        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
+      technologies: [
+        "SFCC",
+        "Javascript",
+        "Jobs framework",
+        "ISML",
+        "Business manager",
+        "SCSS",
+        "jQuery",
+        "Bootstrap",
+        "Demandware",
+        "Bitbucket",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://www.aerin.com/",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "/aerin.mp4",
+    },
+    {
+      title: "Shopify",
+      href: "https://www.shopify.com",
+      dates: "Feb 2021 - May 2021",
+      active: true,
+      description:
+        "With the shopify platform worked with multiple stores to implement solutions and enhance the UI and features on the site",
+      technologies: [
+        "Shopify CMS",
+        "ISML",
+        "Javascript",
+        "jQuery",
+        "SCSS",
+        "CSS",
+        "Bootstrap",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://www.shopify.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "/shopify.mp4",
+    },
+  ],
+  organizations: [
+    {
+      title: "Tata Consultancy and Services",
+      dates: "April 1st - Present, 2024",
+      location: "Mumbai, Maharashtra",
+      description:
+        "Developed ecommerce solutions to enhance site performance, stories and bug fixing using sfcc pwa kit.",
+      image:
+        "https://www.pc-tablet.co.in/wp-content/uploads/2024/04/Tata-Consultancy-Services-TCS.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [
+        { icon: undefined, title: "website ->", href: "https://www.tcs.com/" },
+      ],
+    },
+    {
+      title: "Techouts Solution",
+      dates: "January 1st - March 16th, 2024",
+      location: "Hyderabad, Telangana",
+      description:
+        "Worked with the titan brand to implement stories and fix bugs acrosss all titan sub-sites",
+      image: "https://techoutwebsitebkt.s3.amazonaws.com/Logo_7506f74295.svg",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [
+        {
+          icon: undefined,
+          title: "website ->",
+          href: "https://www.techouts.com/homepage",
+        },
+      ],
+    },
+    {
+      title: "Apexon",
+      dates: "February 21st, 2021 - December 31st, 2023",
+      location: "Thane, Maharashtra",
+      description:
+        "Developed multiple stores which are based on ecommerce in the shopify and salesforce commerce cloud platform as well.",
+      icon: "public",
+      image:
+        "https://awsmp-logos.s3.amazonaws.com/32e441db-99f5-4fe2-bad9-a918cfbdf98a/4d3154acff0600a97231d48a33c7937d.png",
+      links: [
+        {
+          icon: undefined,
+          title: "website ->",
+          href: "https://www.apexon.com/",
+        },
+      ],
+    },
+  ],
+} as const;
