b7a68caac9c065570d84825c6e45da3afd73850d
Shubham Borade <boradeshubham02@gmail.com>
2024-10-05 11:41:15 +0530

    contact text changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -279,8 +279,7 @@ export default function Page() {
                 >
                   with a direct question on WhatsApp
                 </Link>{" "}
-                and I&apos;ll respond whenever I can. I will ignore all
-                soliciting.
+                and I&apos;ll respond whenever I can.
               </p>
             </div>
           </BlurFade>
